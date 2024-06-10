1efe6a911fb423932e53b7ca8e0be70721eb5446
Oumaima Amlou <128989099+Am-umaima@users.noreply.github.com>
2024-06-10 18:00:10 +0100

    Update calculator.js

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -20,7 +20,7 @@ function calculate() {
         if (khabiza.test(input)) {
             result = eval(input);
 			document.getElementById("result").style.border="none" ;
-			document.getElementById("warning").style.visibility  = "visible";
+			document.getElementById("warning").style.visibility  = "none";
         } else {
             result ='';
 			document.getElementById("result").style.border="red 3px solid" ;
